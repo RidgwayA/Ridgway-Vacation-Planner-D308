7bf8c4782f7b9083a3ffb7ac393bb98ebea30dc9
RidgwayA <52265069+RidgwayA@users.noreply.github.com>
2024-12-17 13:40:27 -0600

    Changed a few names and added views to match requirements in in part C. And added adapters to expand upon later.

---
+++ app/src/main/java/com/example/ridgwayvacationplanner_d308/UI/VacationAdapter.java
@@ -0,0 +1,4 @@
+package com.example.ridgwayvacationplanner_d308.UI;
+
+public class VacationAdapter {
+}
--- app/src/main/java/com/example/ridgwayvacationplanner_d308/UI/VacationDetails.java
+++ app/src/main/java/com/example/ridgwayvacationplanner_d308/UI/VacationDetails.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ridgwayvacationplanner_d308.R;
 
-public class ExcursionList extends AppCompatActivity {
+public class VacationDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_excursion_list);
+        setContentView(R.layout.activity_vacation_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

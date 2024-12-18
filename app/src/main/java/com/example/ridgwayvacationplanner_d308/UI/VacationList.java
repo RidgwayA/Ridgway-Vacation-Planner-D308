273006ebce2f23d9743345dea826a391bff7bfde
RidgwayA <52265069+RidgwayA@users.noreply.github.com>
2024-12-18 13:09:07 -0600

    Setup vacationAdapter and vacationList so you can select sample data from a menu and have it populate the database and show on the recycler view on vacationList. This aligns with section B and being able to view all vacations.

--- app/src/main/java/com/example/ridgwayvacationplanner_d308/UI/VacationList.java
+++ app/src/main/java/com/example/ridgwayvacationplanner_d308/UI/VacationList.java
@@ -2,17 +2,22 @@ package com.example.ridgwayvacationplanner_d308.UI;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ridgwayvacationplanner_d308.R;
+import com.example.ridgwayvacationplanner_d308.adapters.VacationAdapter;
 import com.example.ridgwayvacationplanner_d308.database.Repository;
+import com.example.ridgwayvacationplanner_d308.entities.Vacation;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class VacationList extends AppCompatActivity {
     private Repository repository;
@@ -22,21 +27,57 @@ public class VacationList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_vacation_list);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        return insets;
-    });
+        //implement the floating action button
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VacationList.this, VacationDetails.class);
+                startActivity(intent);
+            }
 
+        });
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        repository = new Repository(getApplication());
+        List<Vacation> allVacations = repository.getmAllVacations();
+        final VacationAdapter vacationAdapter = new VacationAdapter(this);
+        recyclerView.setAdapter(vacationAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        vacationAdapter.setVacations(allVacations);
+    }
 
-            //implement the floating action button
-            FloatingActionButton fab = findViewById(R.id.floatingActionButton);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(VacationList.this, VacationDetails.class);
-                    startActivity(intent);
-                }
-            });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.vacation_list, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        List<Vacation> allVacations = repository.getmAllVacations();
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final VacationAdapter vacationAdapter = new VacationAdapter(this);
+        recyclerView.setAdapter(vacationAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        vacationAdapter.setVacations(allVacations);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+    if(item.getItemId() == R.id.mysample)
+
+    {
+        repository = new Repository(getApplication());
+        Vacation vacation = new Vacation(0, "Cruise", "Carnival", "01/01/2025", "01/07/2025");
+        repository.insert(vacation);
+        vacation = new Vacation(0, "Resort", "Sandals", "02/01/2025", "02/07/2025");
+        repository.insert(vacation);
+        return true;
+    }
+    return true;
     }
 }
--- app/src/main/java/com/example/ridgwayvacationplanner_d308/adapters/VacationAdapter.java
+++ app/src/main/java/com/example/ridgwayvacationplanner_d308/adapters/VacationAdapter.java
@@ -1,4 +1,84 @@
 package com.example.ridgwayvacationplanner_d308.adapters;
 
-public class VacationAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ridgwayvacationplanner_d308.R;
+import com.example.ridgwayvacationplanner_d308.UI.VacationDetails;
+import com.example.ridgwayvacationplanner_d308.entities.Vacation;
+
+import java.util.List;
+
+public class VacationAdapter extends RecyclerView.Adapter<VacationAdapter.VacationViewHolder>{
+
+    private List<Vacation> mVacations;
+    private final Context context;
+    private final LayoutInflater mInflater;
+
+    public class VacationViewHolder extends RecyclerView.ViewHolder {
+        private final TextView vacationItemView;
+        public VacationViewHolder(@NonNull View itemView) {
+            super(itemView);
+            vacationItemView = itemView.findViewById(R.id.textViewVacationItem);
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    int position = getAdapterPosition();
+                    final Vacation current = mVacations.get(position);
+                    Intent intent = new Intent(context, VacationDetails.class);
+                    intent.putExtra("Vacation ID", current.getVacationID());
+                    intent.putExtra("vacation Name", current.getVacationTitle());
+                    intent.putExtra("Hotel Name", current.getVacationLodging());
+                    intent.putExtra("Start Date", current.getStartDate());
+                    intent.putExtra("End Date", current.getEndDate());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public VacationAdapter.VacationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.vacation_list_item,parent,false);
+        return new VacationViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VacationAdapter.VacationViewHolder holder, int position) {
+        if(mVacations != null){
+            Vacation current = mVacations.get(position);
+            String name = current.getVacationTitle();
+            holder.vacationItemView.setText(name);
+        }
+        else{
+            holder.vacationItemView.setText("No Vacation Name");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mVacations != null) {
+            return mVacations.size();
+        }
+        else return 0;
+    }
+
+    public VacationAdapter(Context context){
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    public void setVacations(List<Vacation> vacations){
+        mVacations = vacations;
+        notifyDataSetChanged();
+    }
+
 }
